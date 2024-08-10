61cdfd138938e0923e492e45d9ebdd95b90d088e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-10 18:14:35 +0530

    stack

--- main.rs
+++ main.rs
@@ -1,23 +1,13 @@
 fn main() {
-    let float_num: f32 = 3.14;
-    let float_num_2: f64 = 3.1234;
+    let x: u32 = 10;
+    let y: u32 = x;
 
-    let tup: (i32, f64, i32, &str) = (20, 3.12, 1, "hello");
+    let sum = sum(x, y);
 
-    println!("{}", tup.1);
+    println!("{}", sum);
+}
 
-    let (a, b, c, d) = tup; //tuples
-    println!("{}", a);
-    println!("{}", b);
-    println!("{}", c);
-    println!("{}", d);
-
-    let x: [i32; 4] = [1, 5, 6, 7];
-
-    println!("{}", x[3]);
-
-    let y: [i32; 6] = [2; 6]; // [2,2,2,2,2,2]
-
-    println!("{}", y[6]);
-
+fn sum(x: u32, y: u32) -> u32 {
+    let sum: u32 = x + y;
+    sum
 }
