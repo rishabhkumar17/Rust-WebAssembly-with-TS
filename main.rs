0018a1ea2e34da99cc75a360a8d8678d2ed84bf3
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-10 17:13:27 +0530

    tuples and array

--- main.rs
+++ main.rs
@@ -1,24 +1,23 @@
 fn main() {
-    let message: &str = "Hello World";
-    let message_2: &str = print_welcome(message);
-    println!("{}", message_2);
+    let float_num: f32 = 3.14;
+    let float_num_2: f64 = 3.1234;
 
-    let custom_num: i32 = 90_000;
-    let hex_num: i32 = 0xfa; //0x for hex decimal (250)
-    let bin_num: i32 = 0b0010_1011; //0b for binary (43)
-    let byte_num: u8 = b'A'; //b for byte (utf-8) ( hexadecimal 0x41  to decimal 65)
-    let bool_num: bool = true;
-    let eight_bit: u8 = 255;
+    let tup: (i32, f64, i32, &str) = (20, 3.12, 1, "hello");
 
-    println!("{}", custom_num);
-    println!("{}", hex_num);
-    println!("{}", bin_num);
-    println!("{}", byte_num);
-    println!("{}", bool_num);
-    println!("{}", eight_bit);
-}
+    println!("{}", tup.1);
 
-fn print_welcome(text: &str) -> &str {
-    println!("{}", text);
-    "Hi There"
+    let (a, b, c, d) = tup; //tuples
+    println!("{}", a);
+    println!("{}", b);
+    println!("{}", c);
+    println!("{}", d);
+
+    let x: [i32; 4] = [1, 5, 6, 7];
+
+    println!("{}", x[3]);
+
+    let y: [i32; 6] = [2; 6]; // [2,2,2,2,2,2]
+
+    println!("{}", y[6]);
+
 }
