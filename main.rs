99b3c8d708584cde182bf031e12709536ac35573
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-12 22:45:13 +0530

    return moved value

--- main.rs
+++ main.rs
@@ -1,17 +1,10 @@
 fn main() {
-    let message: String = String::from("Hello"); // message coming into the scope
-    print_message(message); // message is moved into this function
-    // message is not longer valid
-}
-// message is going out of scope, but nothing will happen because it was moved
-
-fn print_message(a: String) { // a comes into the scope
-    println!("{}", a); //prints
-    let c: String = a; // c is coming into the scope and a is moved into c;
-    println!("{}", c); //prints
-    // a is no longer valid;
+    let message: String = String::from("Hello");
+    let message_2 = extend_message(message);
+    println!("{}", message_2);
 }
 
-// a is going out of scope, but nothing will happen because it was moved
-
-// c is going out of scope and "drop" is called which clears the memory from the heap
+fn extend_message(mut a: String) -> String {
+    a.push_str(" World!");
+    a
+}
