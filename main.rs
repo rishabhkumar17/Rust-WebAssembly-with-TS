b4cf80688a452dff358508828a91af0f461f552b
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-12 22:02:41 +0530

    string move

--- main.rs
+++ main.rs
@@ -8,13 +8,16 @@ fn main() {
 
     let message: &str = "Hello";
 
-    let message2: String = String::from("Hello_2"); // heap
+    let message_2: String = String::from("Hello_2"); // heap
+
+    let message_3: String = message_2; // copy pointer
+
+    println!("{}", message_3);
 }
 
 fn sum(x: u32, y: u32) -> u32 {
     let sum: u32 = x + y;
     sum
 }
-
 
 // xxd -g 1 -c 8 main  | display binary files
