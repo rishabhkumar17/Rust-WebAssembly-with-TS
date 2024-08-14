79c1b4dc6327885357911200796232785724bf4c
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-15 00:43:16 +0530

    mutable borrow #2

--- main.rs
+++ main.rs
@@ -2,8 +2,11 @@ fn main() {
     let mut message: String = String::from("hello");
     let message_2: &mut String = &mut message; // mutable borrow
 
-    message_2.push_str(" world");
-
+    unpredictable_mutate(message_2);  
     println!("{}", message);
     // println!("{}", message_2);
+}
+
+fn unpredictable_mutate(val: &mut String) {
+    val.push_str("_unpredictable");
 }
