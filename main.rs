77f8215cfe64f80579958ad53224b4da45460d43
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-15 02:24:03 +0530

    double dereferencing #2

--- main.rs
+++ main.rs
@@ -1,8 +1,18 @@
 fn main() {
     let a: i32 = 27;
     let b: &i32 = &a;
-    let c: &&i32 = &b;
+    let mut c: &&i32 = &b;
+    let d: &i32 = b;
 
-    println!("{}", a == *b); // dereferencing b using *
-    println!("{}", a == **c);
+    let e: &&i32 = &&17;
+    c = e;
+
+    // println!("{:p}", &a);
+    // println!("{:p}", b);
+    println!("{:p}", c);
+    println!("{:p}", *c);
+    // println!("{:p}", d);
+
+    println!("{:p}", e);
+    println!("{:p}", &(**e));
 }
