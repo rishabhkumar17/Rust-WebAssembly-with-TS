6ed2a45d5882f71369a4b5f23157733526737439
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-15 00:20:12 +0530

    reference

--- main.rs
+++ main.rs
@@ -1,20 +1,10 @@
 fn main() {
-    let message: String = String::from("Hello");
-    let message_2 = extend_message(message);
-    println!("{}", message_2);
+   let message: String = String::from("hello");
+   let message_2: &String = &message; // pointer
+    // message_2 is not owner of data
+    // message_2 is "borrowing" a reference to message
 
-    let age: u32 = 27;
-    let age_2: u32 = extend_age(age);
-    println!("{}", age);
-    println!("{}", age_2);
+   println!("{}", message);
+   println!("{}", message_2); 
 }
 
-fn extend_message(mut a: String) -> String {
-    a.push_str(" World!");
-    a
-}
-
-fn extend_age(mut a: u32) -> u32 {
-    a += 1;
-    a
-}
