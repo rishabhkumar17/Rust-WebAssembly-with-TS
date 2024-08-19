2a89a6eb22705c162573dceb5c0982f82d05e68b
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-19 15:15:46 +0530

    string slice

--- main.rs
+++ main.rs
@@ -1,18 +1,6 @@
 fn main() {
-    let a: i32 = 27;
-    let b: &i32 = &a;
-    let mut c: &&i32 = &b;
-    let d: &i32 = b;
+    let message: String = String::from("hello");
+    let slice: &str = &message[2..4]; //slice (2 -> 3) 2 is inclusive and 4 is exclusive
 
-    let e: &&i32 = &&17;
-    c = e;
-
-    // println!("{:p}", &a);
-    // println!("{:p}", b);
-    println!("{:p}", c);
-    println!("{:p}", *c);
-    // println!("{:p}", d);
-
-    println!("{:p}", e);
-    println!("{:p}", &(**e));
+    println!("{}", slice);
 }
