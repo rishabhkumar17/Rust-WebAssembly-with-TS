c78e5b3dc87f04e80e53751414a2a7d1a355e30e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-19 16:25:17 +0530

    constuctors

--- main.rs
+++ main.rs
@@ -4,6 +4,18 @@ struct Person {
 }
 
 impl Person {
+    //constructors
+    fn new() -> Person {
+        Person {
+            name: "Default".to_string(),
+            age: 0,
+        }
+    }
+
+    fn from(name: String, age: u32) -> Person {
+        Person { name, age }
+    }
+
     // associated function -> can only be assesed by struct
     fn some_function() {
         println!("Yeah Buddy!")
@@ -12,20 +24,25 @@ impl Person {
     //method
     // first parameter is always self which represent the instance of struct, the method is being called on
     // Within an impl block, the type Self is an alias for current type
-    fn change_age(&self) {
+    fn display_age(&self) {
         println!("Current age: {}", self.age);
+    }
+
+    fn change_age(&mut self, new_age: u32) {
+        self.age = new_age;
     }
 }
 
 fn main() {
     Person::some_function();
 
-    let person: Person = Person {
-        name: "sage".to_string(),
-        age: 27,
-    };
+    let mut person: Person = Person::new();
+    let person_2: Person = Person::from("sage".to_string(), 17);
 
-    person.change_age();
+    person.display_age();
+
+    person.change_age(28);
 
     println!("{} {}", person.name, person.age);
+    println!("{} {}", person_2.name, person_2.age);
 }
