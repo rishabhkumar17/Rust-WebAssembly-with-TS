cb89f0648ad1f1cc2467f53b54f3aed1075350ed
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-19 16:08:13 +0530

    struct

--- main.rs
+++ main.rs
@@ -1,9 +1,31 @@
+struct Person {
+    name: String, // fields
+    age: u32,
+}
+
+impl Person {
+    // associated function -> can only be assesed by struct
+    fn some_function() {
+        println!("Yeah Buddy!")
+    }
+
+    //method
+    // first parameter is always self which represent the instance of struct, the method is being called on
+    // Within an impl block, the type Self is an alias for current type
+    fn change_age(&self) {
+        println!("Current age: {}", self.age);
+    }
+}
+
 fn main() {
-    let mut message: String = String::from("hello");
-    let message_2: String = message.clone(); // deep clone
+    Person::some_function();
 
-    message.clear();
+    let person: Person = Person {
+        name: "sage".to_string(),
+        age: 27,
+    };
 
-    println!("message is {:}", message);
-    println!("{}", message_2);
+    person.change_age();
+
+    println!("{} {}", person.name, person.age);
 }
