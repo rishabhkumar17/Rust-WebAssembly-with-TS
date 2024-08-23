b66f53d87746ebdec32297fb575a95e831a4db30
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-08-23 18:15:19 +0530

    modularity

--- main.rs
+++ main.rs
@@ -1,76 +1,7 @@
-// trait
-
-trait Log {
-    fn display_info(&self);
-    fn alert_something(&self) {
-        println!("Default")
-    }
-}
-
-#[derive(Debug)]
-enum PersonId {
-    // either Passport or IdentityCard
-    Passport(String),
-    IdentityCard(String, u32, u32),
-}
-
-struct Person {
-    name: String, // fields
-    age: u32,
-    id: PersonId,
-}
-
-impl Log for Person {
-    fn display_info(&self) {
-        println!("{} {} {:?}", self.name, self.age, self.id);
-    }
-}
-
-struct Animal(String, u32, String); // no fields
-
-impl Log for Animal {
-    fn display_info(&self) {
-        println!("{}", self.0);
-    }
-    fn alert_something(&self) {
-        println!("Default Animal");
-    }
-}
-
-impl Person {
-    //constructors
-    fn new() -> Person {
-        Person {
-            name: "Default".to_string(),
-            age: 0,
-            id: PersonId::IdentityCard("ID".to_string(), 1, 2),
-        }
-    }
-
-    fn from(name: String, age: u32, id: PersonId) -> Person {
-        Person { name, age, id }
-    }
-
-    // associated function -> can only be assesed by struct
-    fn some_function() {
-        println!("Yeah Buddy!")
-    }
-
-    //method
-    // first parameter is always self which represent the instance of struct, the method is being called on
-    // Within an impl block, the type Self is an alias for current type
-    fn display_age(&self) {
-        println!("Current age: {}", self.age);
-    }
-
-    fn change_age(&mut self, new_age: u32) {
-        self.age = new_age;
-    }
-
-    // fn display_info(&self) {
-    //     println!("{} {} {:?}", self.name, self.age, self.id);
-    // }
-}
+use rust_webassembly_with_ts::logger;
+use rust_webassembly_with_ts::logger_2;
+use rust_webassembly_with_ts::Animal;
+use rust_webassembly_with_ts::Person;
 
 fn main() {
     let person = Person::new();
@@ -83,15 +14,4 @@ fn main() {
     logger(person);
 
     logger_2(&animal);
-}
-
-// impl makes the compiler determine type at the compile time
-// it will create mutiple versions of the funtion depends how many types Log traits implements(Animal, Person)
-fn logger(val: impl Log) {
-    val.alert_something();
-}
-
-// dyn is short for dynamic, function performs dynamic dispatch -> dicision of exactly which function to call at the run time
-fn logger_2(val: &dyn Log) {
-    val.alert_something();
 }
