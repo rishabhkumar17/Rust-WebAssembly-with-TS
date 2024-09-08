2305b07306d1a31bc048670c2f1ed84b588722b1
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-09-08 21:41:26 +0530

    init code

--- www/index.js
+++ www/index.js
@@ -1,35 +1,6 @@
-async function init() {
-    // const byteArray = new Int8Array([0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01, 0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07, 0x07, 0x01, 0x03, 0x73, 0x75, 0x6d, 0x00, 0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b, 0x00, 0x18, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x01, 0x06, 0x01, 0x00, 0x03, 0x73, 0x75, 0x6d, 0x02, 0x09, 0x01, 0x00, 0x02, 0x00, 0x01, 0x61, 0x01, 0x01, 0x62,]);
+import init, { greet } from "rust_webassembly_with_ts";
 
-    const memory = new WebAssembly.Memory({ initial: 1 });
-
-    const importObject = {
-        js: {
-            mem: memory
-        },
-        console: {
-            log: () => {
-                console.log("Logging");
-            },
-            error: () => {
-                console.log("Error");
-            }
-        }
-    }
-
-    const response = await fetch("sum.wasm");
-    const buffer = await response.arrayBuffer();
-
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
-    const sumFunc = wasm.instance.exports.sum;
-    debugger
-    // const wasmMemory = wasm.instance.exports.mem;
-    const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-    const hiText = new TextDecoder().decode(uint8Array);
-    console.log(hiText);
-
-    const result = sumFunc(17, 18);
-    console.log(result);
-}
-
-init();
+init().then(_ => {
+    greet("Filip");
+    console.log("Yeah!")
+})
