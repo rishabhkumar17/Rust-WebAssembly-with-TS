66fb88a20cac36624ab568003cbd0405508b5ad2
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-09-12 10:49:54 +0530

    pkg files

--- www/index.js
+++ www/index.js
@@ -1,6 +1,15 @@
 import init, { greet } from "rust_webassembly_with_ts";
 
-init().then(_ => {
-    greet("Filip");
+// async function start() {
+//     const wasm = await init();
+//     wasm.greet("sage");
+//     console.log("OK");
+// }
+
+// await start();
+
+
+init().then(wasm => {
+    wasm.greet("Filip");
     console.log("Yeah!")
 })
