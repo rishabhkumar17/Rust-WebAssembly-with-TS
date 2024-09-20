6d67dcbce61adb8308d88bf577297f1cc17d3fa9
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-09-20 09:52:54 +0530

    wee_alloc

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rust-WebAssembly-with-TS
 
 use `xxd -g 1 -c 8 main` to display binary files
+
+wasm-pack build --target web
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
@@ -9,3 +13,5 @@ pub fn greet(name: &str) {
 extern {
     pub fn alert(s: &str);
 }
+
+//wasm-pack build --target web
