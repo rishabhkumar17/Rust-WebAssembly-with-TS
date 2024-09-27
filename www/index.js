60e99432eaa591fc4812296783eb218a3dc596ca
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-09-27 09:48:10 +0530

    Draw World

--- www/index.js
+++ www/index.js
@@ -1,9 +1,32 @@
 import init, { World } from "rust_webassembly_with_ts";
 
 init().then(_ => {
+    const CELL_SIZE = 100;
     const world = World.new();
+    const worldWidth = world.width();
     const canvas = document.getElementById("snake_canvas");
     const ctx = canvas.getContext("2d");
+
+    canvas.height = worldWidth * CELL_SIZE;
+    canvas.width = worldWidth * CELL_SIZE;
+
+    function drawWorld() {
+        ctx.beginPath();
+
+        for (let i = 0; i <= worldWidth; i++) {
+            //vertical line
+            ctx.moveTo(CELL_SIZE * i, 0);
+            ctx.lineTo(CELL_SIZE * i, worldWidth * CELL_SIZE);
+
+            //horizontal line
+            ctx.moveTo(0, CELL_SIZE * i);
+            ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * i);
+        }
+
+        ctx.stroke();
+    }
+
+    drawWorld();
 })
 
 
